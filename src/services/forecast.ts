3869157e910dd1865e806e2e1eb6c498c1c19008
Lucas Vieira <lucsolivier@gmail.com>
2021-01-21 18:02:55 -0300

    Serviço de forecast

---
+++ src/services/forecast.ts
@@ -0,0 +1,77 @@
+import { ForecastPoint, StormGlass } from '@src/clients/stormGlass'
+import { InternalError } from '@src/util/errors/internal-error'
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N'
+}
+
+export interface Beach {
+  name: string
+  position: BeachPosition
+  lat: number
+  lng: number
+  user: string
+}
+
+export interface TimeForecast {
+  time: string
+  forecast: Array<BeachForecast>
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class ForecastProcessingInternalError extends InternalError {
+  constructor(message: string) {
+    super(`Unexpected error during the forecast processing: ${message}`)
+  }
+}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+  public async processForecastForBeaches(beaches: Array<Beach>): Promise<Array<TimeForecast>> {
+    const pointsWithCorrectSources = []
+    try {
+      for (const beach of beaches) {
+        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng)
+        const enrichedBeachData = this.enrichedBeachData(points, beach)
+        pointsWithCorrectSources.push(...enrichedBeachData)
+      }
+      return this.mapForecastByTime(pointsWithCorrectSources)
+    } catch (error) {
+      throw new ForecastProcessingInternalError(error.message)
+    }
+  }
+
+  private enrichedBeachData(points: Array<ForecastPoint>, beach: Beach): Array<BeachForecast> {
+    return points.map((e) => ({
+      ...{},
+      ...{
+        lat: beach.lat,
+        lng: beach.lng,
+        name: beach.name,
+        position: beach.position,
+        rating: 1
+      },
+      ...e
+    }))
+  }
+
+  private mapForecastByTime(forecast: Array<BeachForecast>): Array<TimeForecast> {
+    const forecastByTime: Array<TimeForecast> = []
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time == point.time)
+      if (timePoint) {
+        timePoint.forecast.push(point)
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point]
+        })
+      }
+    }
+    return forecastByTime
+  }
+}
